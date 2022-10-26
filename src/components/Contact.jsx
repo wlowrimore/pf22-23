5f81798dd63a16bf021f2571d88cf2b8be194a2f
William Lowrimore <wlowrimore@gmail.com>
2022-10-26 10:10:07 -0500

    adds fa icons to Contact. Installs React Router

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,18 @@
+import { FaGitSquare } from 'react-icons/fa';
+import { FaLinkedinIn }  from 'react-icons/fa';
+import { FaGlobe }  from 'react-icons/fa';
+import { FaEnvelope } from 'react-icons/fa';
+
 export default function Contact() {
   return(
     <div>
       <h1 className='willow-font text-3xl text-yellow-600 pb-8'>[get_in_touch]</h1>
+      <div className='flex justify-between'>
+        <p className='text-gray-400 text-3xl'><FaGitSquare /></p>
+        <p className='text-gray-400 text-3xl'><FaLinkedinIn /></p>
+        <p className='text-gray-400 text-3xl'><FaGlobe /></p>
+        <p className='text-gray-400 text-3xl'><FaEnvelope /></p>
+      </div>
     </div>
   )
 }
