55a5c0741f01b2b9f898c9e978968dd5135cd566
William Lowrimore <wlowrimore@gmail.com>
2022-10-27 05:40:28 -0500

    adds grid display on contact links with useState for hover:text

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -15,17 +15,58 @@ const Contact = () => {
     setIsHovering(false);
   };
 
-  return(
-    <div className='w-1/2'>
-      <h1 className='willow-font text-3xl text-yellow-600 text-center pb-4'>[get_in_touch]</h1>
-      <p className='willow-font text-gray-200 text-center pb-8'>C'mon what are you waiting for? Get in touch with me, and let's code!</p>
-      <div className='flex justify-around w-1/2 mx-32 pt-6'>
-        <a href='https://github.com/wlowrimore' target='_blank' className='text-gray-400 hover:text-purple-300 hover:cursor-pointer text-3xl' onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}><FaGitSquare />{isHovering && <span className='text-sm'>github</span>}</a>
-        <a href='https://linkedin.com/in/william-lowrimore-21778310' target='_blank' className='text-gray-400 hover:text-purple-300 hover:cursor-pointer text-3xl'><FaLinkedinIn /></a>
-        <a href='https://williamlowrimore.com' target='_blank' className='text-gray-400 hover:text-purple-300 hover:cursor-pointer text-3xl'><FaGlobe /></a>
-        <a href='mailto:wlowrimore@gmail.com' target='_blank' className='text-gray-400 hover:text-purple-300 hover:cursor-pointer text-3xl'><FaEnvelope /></a>
+  return (
+    <>
+      <div className='w-1/2 h-100 pb-12'>
+        <div className='text-center'>
+          <h1 className='willow-font text-3xl text-yellow-600 pb-8'>[get_in_touch]</h1>
+          <p className='willow-font text-gray-200'>C'mon what are you waiting for? Get in touch with me, and let's code!</p>
+        </div>
       </div>
-    </div>
+
+      <div className='grid grid-cols-12 h-[50px] w-100 ml-14'>
+        <div className='grid col-span-3'>
+          <a href='https://github.com/wlowrimore' 
+            target='_blank' 
+            className='text-gray-400 hover:text-purple-300 hover:cursor-pointer text-3xl' 
+            onMouseOver={handleMouseOver} 
+            onMouseOut={handleMouseOut}>
+            <FaGitSquare />
+            {isHovering && <p className='text-xs pt-3'>github</p>}
+          </a>
+        </div>
+        <div className='grid col-span-3'>
+          <a href='https://linkedin.com/in/william-lowrimore-21778310' 
+            target='_blank' 
+            className='text-gray-400 hover:text-purple-300 hover:cursor-pointer text-3xl'
+            onMouseOver={handleMouseOver} 
+            onMouseOut={handleMouseOut}>
+            <FaLinkedinIn />
+            {isHovering && <p className='text-xs pt-3'>linkedin</p>}
+          </a>
+        </div>
+        <div className='grid col-span-3'>
+          <a href='https://williamlowrimore.com' 
+            target='_blank' 
+            className='text-gray-400 hover:text-purple-300 hover:cursor-pointer text-3xl' 
+            onMouseOver={handleMouseOver} 
+            onMouseOut={handleMouseOut}>
+            <FaGlobe />
+            {isHovering && <p className='text-xs pt-3'>my site</p>}
+          </a>
+        </div>
+        <div className='grid col-span-1'>
+          <a href='mailto:wlowrimore@gmail.com' 
+            target='_blank' 
+            className='text-gray-400 hover:text-purple-300 hover:cursor-pointer text-3xl' 
+            onMouseOver={handleMouseOver} 
+            onMouseOut={handleMouseOut}>
+            <FaEnvelope />
+            {isHovering && <p className='text-xs pt-3'>email</p>}
+          </a>
+        </div>
+      </div>
+    </>
   );
 };
 
