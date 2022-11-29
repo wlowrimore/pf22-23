8a3c08e8008b1c290c5b02c325c546e580a7680c
William Lowrimore <wlowrimore@gmail.com>
2022-11-29 13:26:48 -0600

    adds delishMeals project

--- src/pages/ProjectData.js
+++ src/pages/ProjectData.js
@@ -1,75 +1,82 @@
-import { NavLink } from 'react-router-dom';
-import AllProjects from './AllProjects';
+import { NavLink } from "react-router-dom";
+import AllProjects from "./AllProjects";
 
 // Data to be injected into 'AllProjects' card
 export default function ProjectData() {
   return (
-    <div className='h-screen w-5/6 md:w-full py-28'>
-      <h1 className='willow-font text-xl md:text-3xl text-yellow-600 text-center pb-8'>[my_work].cont</h1>
-      <div className='grid grid-cols-1 gap-8'>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-4 place-self-center'>
+    <div className="h-screen w-5/6 md:w-full py-28">
+      <h1 className="willow-font text-xl md:text-3xl text-yellow-600 text-center pb-8">
+        [my_work].cont
+      </h1>
+      <div className="grid grid-cols-1 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 place-self-center">
           <AllProjects
-            id = 'vet'
-            img = {require('../images/vetPic.png')}
-            repo = "https://github.com/wlowrimore/react_interface"
-            live = "https://react-interface-mu.vercel.app/"
+            id="vet"
+            img={require("../images/vetPic.png")}
+            repo="https://github.com/wlowrimore/react_interface"
+            live="https://react-interface-mu.vercel.app/"
           />
           <AllProjects
-            id = 'code'
-            img = {require('../images/codePic.png')}
-            repo = "https://github.com/wlowrimore/vandy_bc_code_quiz_chlng4_2022"
-            live = "https://wlowrimore.github.io/vandy_bc_code_quiz_chlng4_2022/"
+            id="meals"
+            img={require("../images/foodPic.png")}
+            repo="https://github.com/wlowrimore/react_food_app"
+            live="https://delishmeals.vercel.app/"
           />
-          <AllProjects 
-            id = 'expense'
-            img = {require('../images/expensePic.png')}
-            repo = 'https://github.com/wlowrimore/udemy_react_project'
-            live = "https://udemy-react-project.vercel.app/"
+          <AllProjects
+            id="code"
+            img={require("../images/codePic.png")}
+            repo="https://github.com/wlowrimore/vandy_bc_code_quiz_chlng4_2022"
+            live="https://wlowrimore.github.io/vandy_bc_code_quiz_chlng4_2022/"
           />
-          <AllProjects 
-            id = 'tech'
-            img = {require('../images/techPic.png')}
-            repo = 'Vandy-bc-2022-MVC-Tech-Blog-Chlng-14'
-            live = "https://serene-springs-59438.herokuapp.com/"
+          <AllProjects
+            id="expense"
+            img={require("../images/expensePic.png")}
+            repo="https://github.com/wlowrimore/udemy_react_project"
+            live="https://udemy-react-project.vercel.app/"
           />
-          <AllProjects 
-            id = 'bounty'
-            img = {require('../images/bountyPic.png')}
-            repo = 'https://github.com/wlowrimore/vandy_bc_project3_bounty-book_MERN_2022'
-            live = "https://bountybook.herokuapp.com/"
+          <AllProjects
+            id="tech"
+            img={require("../images/techPic.png")}
+            repo="Vandy-bc-2022-MVC-Tech-Blog-Chlng-14"
+            live="https://serene-springs-59438.herokuapp.com/"
           />
-          <AllProjects 
-            id = 'budget'
-            img = {require('../images/budgetPic.png')}
-            repo = 'https://github.com/wlowrimore/Vandy-bc-PWA-Budget-Tracker-Chlng-19-2022'
-            live = "https://tranquil-shore-61237.herokuapp.com/"
+          <AllProjects
+            id="bounty"
+            img={require("../images/bountyPic.png")}
+            repo="https://github.com/wlowrimore/vandy_bc_project3_bounty-book_MERN_2022"
+            live="https://bountybook.herokuapp.com/"
           />
-          <AllProjects 
-            id = 'scheduler'
-            img = {require('../images/schedulerPic.png')}
-            repo = 'https://github.com/wlowrimore/vandy-bc-weeklyp-2022'
-            live = "https://wlowrimore.github.io/vandy-bc-weeklyp-2022/"
+          <AllProjects
+            id="budget"
+            img={require("../images/budgetPic.png")}
+            repo="https://github.com/wlowrimore/Vandy-bc-PWA-Budget-Tracker-Chlng-19-2022"
+            live="https://tranquil-shore-61237.herokuapp.com/"
           />
-          <AllProjects 
-            id = 'next'
-            img = {require('../images/nextPic.png')}
-            repo = 'https://github.com/wlowrimore/portfolio-082022'
-            live = "https://github.com/wlowrimore/portfolio-082022"
+          <AllProjects
+            id="scheduler"
+            img={require("../images/schedulerPic.png")}
+            repo="https://github.com/wlowrimore/vandy-bc-weeklyp-2022"
+            live="https://wlowrimore.github.io/vandy-bc-weeklyp-2022/"
           />
-          <AllProjects 
-            id = 'family'
-            img = {require('../images/familyPic.png')}
-            repo = 'https://github.com/wlowrimore/family_photo_app'
-            live = "https://github.com/wlowrimore/family_photo_app"
+          <AllProjects
+            id="next"
+            img={require("../images/nextPic.png")}
+            repo="https://github.com/wlowrimore/portfolio-082022"
+            live="https://github.com/wlowrimore/portfolio-082022"
+          />
+          <AllProjects
+            id="family"
+            img={require("../images/familyPic.png")}
+            repo="https://github.com/wlowrimore/family_photo_app"
+            live="https://github.com/wlowrimore/family_photo_app"
           />
         </div>
       </div>
-      <div className='view-link mt-10'>
-        <li className='view-resume-style pb-10'><NavLink to='/resume'>view resume</NavLink></li>
+      <div className="view-link mt-10">
+        <li className="view-resume-style pb-10">
+          <NavLink to="/resume">view resume</NavLink>
+        </li>
       </div>
     </div>
   );
-};
-
-    
-    
+}
